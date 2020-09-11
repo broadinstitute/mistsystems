5221e2b82084cc61df462e1df0ebde6c9c4eb47c
Thomas Munzer <thomas.munzer@gmail.com>
2020-09-11 14:50:56 +0200

    update rogue requests

--- mistsystems/requests/sites/rogues.py
+++ mistsystems/requests/sites/rogues.py
@@ -3,26 +3,91 @@ class Rogues():
     def __init__(self, session):
         self.session = session
 
-    def set_rogue(self, site_id, rogue_param):
-        uri = "/api/v1/sites/%s/setting" % site_id
-        resp = self.session.mist_post(uri, body=rogue_param)
-        return resp
-
-    def get(self, site_id, duration="1d", r_type="others", page=1, limit=100):
+    def get_aps(self, site_id, duration=None, rogue_type=None, page=1, limit=100):
+        """
+        Get list of rogue APs
+        Parameters:
+            site_id: String
+            duraton: String (optional. 1d or 1h, default is 1d)
+            rogue_type: String (optional, honeypot, lan, others, spoof. Default is others)
+            page: Int
+            limit: Int
+        """
         uri = "/api/v1/sites/%s/insights/rogues" % site_id
-        query = {"duration": duration, "type": r_type}
+        query = {}
+        if duration: query["duration"]=duration
+        if rogue_type: query["type"]=rogue_type
         resp = self.session.mist_get(uri, query=query, page=page, limit=limit)
         return resp
 
-    def report(self, site_id, r_type, fields):
-        rogues = get(self, site_id)
-        result = []
-        for rogue in rogues['result']["results"]:
-            temp = []
-            for field in fields:
-                if field not in rogue:
-                    temp.append("")
-                else:
-                    temp.append("%s" % rogue[field])
-            result.append(temp)
-        return result
+    def get_clients(self, site_id, duration=None, page=1, limit=100):
+        """
+        Get list of rogue APs
+        Parameters:
+            site_id: String
+            duraton: String (optional. 1d or 1h, default is 1d)
+            page: Int
+            limit: Int
+        """
+        uri = "/api/v1/sites/%s/insights/rogues/clients" % site_id
+        query = {}
+        if duration: query["duration"]=duration
+        resp = self.session.mist_get(uri, query=query, page=page, limit=limit)
+        return resp
+
+    def get_ap_details(self, site_id, rogue_bssid):
+        """
+        Get list of rogue APs
+        Parameters:
+            site_id: String
+            rogue_bssid: String            
+        """
+        uri = "/api/v1/sites/{0}/rogues/{1}".format(site_id, rogue_bssid)
+        resp = self.session.mist_get(uri)
+        return resp
+
+    def search_events(self, site_id, duration=None, rogue_type=None, page=1, limit=100):
+        """
+        search list of rogue APs events
+        Parameters:
+            site_id: String
+            duraton: String (optional. 1d or 1h, default is 1d)
+            rogue_type: String (optional, honeypot, lan, others, spoof. Default is others)
+            page: Int
+            limit: Int
+        """
+        uri = "/api/v1/sites/{0}/rogues/events/search".format(site_id)
+        query = {}
+        if duration: query["duration"]=duration
+        if rogue_type: query["type"]=rogue_type
+        resp = self.session.mist_get(uri, query=query, page=page, limit=limit)
+        return resp
+
+    def count_events(self, site_id, duration=None, rogue_type=None, page=1, limit=100):
+        """
+        count list of rogue APs events
+        Parameters:
+            site_id: String
+            duraton: String (optional. 1d or 1h, default is 1d)
+            rogue_type: String (optional, honeypot, lan, others, spoof. Default is others)
+            page: Int
+            limit: Int
+        """
+        uri = "/api/v1/sites/{0}/rogues/events/count".format(site_id)
+        query = {}
+        if duration: query["duration"]=duration
+        if rogue_type: query["type"]=rogue_type
+        resp = self.session.mist_get(uri, query=query, page=page, limit=limit)
+        return resp
+
+    
+    def deauth_clients(self, site_id, rogue_bssid):
+        """
+        Send Deauth frame to clients connected to a Rogue AP
+        Parameters:
+            site_id: String
+            rogue_bssid: String
+        """
+        uri = "/api/v1/sites/{0}/rogues/{1}/deauth_clients".format(site_id, rogue_bssid)
+        resp = self.session.mist_post(uri)
+        return resp
