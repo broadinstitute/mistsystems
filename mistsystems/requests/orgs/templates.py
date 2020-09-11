cba95e95c629a30575f2cd94cd14d6f8b625bf26
Thomas Munzer <thomas.munzer@gmail.com>
2020-09-11 19:31:51 +0200

    add requests

--- mistsystems/requests/orgs/templates.py
+++ mistsystems/requests/orgs/templates.py
@@ -18,6 +18,17 @@ class Templates():
         resp = self.session.mist_post(uri, body=template_settings)
         return resp
 
+    def update(self, org_id, template_id, template_settings):
+        uri = "/api/v1/orgs/{0}/templates/{1}".format(org_id, template_id)
+        resp = self.session.mist_put(uri, body=template_settings)
+        return resp
+
+    def clone(self, org_id, template_id, name):
+        uri = "/api/v1/orgs/{0}/templates/{1}/clone".format(
+            org_id, template_id)
+        resp = self.session.mist_post(uri, body={"name": name})
+        return resp
+
     def delete(self, org_id, template_id):
         uri = "/api/v1/orgs/%s/templates/%s" % (org_id, template_id)
         resp = self.session.mist_delete(uri)
