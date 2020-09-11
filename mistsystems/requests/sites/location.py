639c4421383eb31ebf7711d5d647ff0db5a9b391
Thomas Munzer <thomas.munzer@gmail.com>
2020-09-11 12:26:45 +0200

    adding location requests

--- mistsystems/requests/sites/location.py
+++ mistsystems/requests/sites/location.py
@@ -1,9 +1,15 @@
 class Location():
-
     def __init__(self, session):
         self.session = session
 
-    def reset(self, site_id, map_id):
-        uri = "/api/v1/sites/%s/location/ml/reset/map/%s" % (site_id, map_id)
-        resp = self.session.mist_post(uri)
+    def get_sdk_stats(self, site_id, map_id):
+        uri = "/api/v1/sites/{0}/stats/maps/{1}/sdkclients".format(
+            site_id, map_id)
+        resp = self.session.mist_get(uri)
+        return resp
+
+    def get_sdk_stats_details(self, site_id, map_id, sdkclient_uuid):
+        uri = "/api/v1/sites/{0}/stats/maps/{1}/sdkclients/{2}".format(
+            site_id, map_id, sdkclient_uuid)
+        resp = self.session.mist_get(uri)
         return resp
