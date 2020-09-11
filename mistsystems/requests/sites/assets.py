b24c11e67c1c1f29d3dc7bcfdd8f5de5ae97a222
Thomas Munzer <thomas.munzer@gmail.com>
2020-09-11 09:28:51 +0200

    updating requests

--- mistsystems/requests/sites/assets.py
+++ mistsystems/requests/sites/assets.py
@@ -4,22 +4,113 @@ class Assets():
         self.session = session
 
     def create(self, site_id, asset_settings):
-        uri = "/api/v1/sites/%s/assets" % site_id
+        """
+        Create site asset 
+        Parameters:
+            site_id: String
+            asset_settings: Dict
+                name|string|name / label of the device
+                mac|string|bluetooth MAC
+        """
+        uri = "/api/v1/sites/{0}/assets".format(site_id)
         body = asset_settings
         resp = self.session.mist_post(uri, body=body)
         return resp
 
-    def update(self, site_id, asset_id, body={}):
-        uri = "/api/v1/sites/%s/assets/%s" % (site_id, asset_id)
-        resp = self.session.mist_put(uri, body=body)
+    def update(self, site_id, asset_id, asset_settings):
+        """
+        Update site asset settings
+        Parameters:
+            site_id: String
+            asset_id: String
+            asset_settings: Dict
+                name|string|name / label of the device
+                mac|string|bluetooth MAC
+        """
+        uri = "/api/v1/sites/{0}/assets/{1}".format(site_id, asset_id)
+        resp = self.session.mist_put(uri, body=asset_settings)
         return resp
 
     def delete(self, site_id, asset_id):
-        uri = "/api/v1/sites/%s/assets/%s" % (site_id, asset_id)
+        """
+        Delete site asset
+        Parameters:
+            site_id: String
+            asset_id: String
+        """
+        uri = "/api/v1/sites/{0}/assets/{1}".format(site_id, asset_id)
         resp = self.session.mist_delete(uri)
         return resp
 
-    def get(self, site_id, page=1, limit=100):
-        uri = "/api/v1/sites/%s/assets" % site_id
+    def get(self, site_id, query={}, page=1, limit=100):
+        """
+        Get the list of the site assets
+        Parameters:
+            site_id: String
+            query: Dict (dict of search filters)
+            page: Int (pagination page)
+            limit: Int (maximum number of entries per request)
+        """
+        uri = "/api/v1/sites/{0}/assets".format(site_id)
+        resp = self.session.mist_get(uri, query=query, page=page, limit=limit)
+        return resp
+
+    def getDetails(self, site_id, asset_id):
+        """
+        Get the list of the site asset filters
+        Parameters:
+            site_id: String
+            asset_id: String
+        """
+        uri = "/api/v1/sites/{0}/assets/{1}".format(site_id, asset_id)
+        resp = self.session.mist_get(uri)
+        return resp
+
+    def getStats(self, site_id, page=1, limit=100):
+        """
+        Get stats of the site assets
+        Parameters:
+            site_id: String
+            page: Int (pagination page)
+            limit: Int (maximum number of entries per request)
+        """
+        uri = "/api/v1/sites/{0}/stats/assets".format(site_id)
         resp = self.session.mist_get(uri, page=page, limit=limit)
         return resp
+
+    def getStatsDetails(self, site_id, asset_id):
+        """
+        Get stats of one site asset
+        Parameters:
+            site_id: String
+            asset_id: String
+        """
+        uri = "/api/v1/sites/{0}/stats/assets/{1}".format(site_id, asset_id)
+        resp = self.session.mist_get(uri)
+        return resp
+
+    def getDiscoveredAsset(self, site_id, page=1, limit=100):
+        """
+        Get stats of the dscovered site assets
+        Parameters:
+            site_id: String
+            page: Int (pagination page)
+            limit: Int (maximum number of entries per request)
+        """
+        uri = "/api/v1/sites/{0}/stats/discovered_assets".format(site_id)
+        resp = self.session.mist_get(uri, page=page, limit=limit)
+        return resp
+
+    def getDiscoveredAssetByMap(self, site_id, map_id, page=1, limit=100):
+        """
+        Get stats of the dscovered site assets by map
+        Parameters:
+            site_id: String
+            map_id: String
+            page: Int (pagination page)
+            limit: Int (maximum number of entries per request)
+        """
+        uri = "/api/v1/sites/{0}/stats/maps/{1}/discovered_assets".format(site_id, map_id)
+        resp = self.session.mist_get(uri, page=page, limit=limit)
+        return resp
+
