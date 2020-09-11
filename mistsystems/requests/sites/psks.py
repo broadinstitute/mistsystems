040e1cc4bf7f9235fe75b1319d77b0bb2ee804b6
Thomas Munzer <thomas.munzer@gmail.com>
2020-09-11 14:37:04 +0200

    update psk requests

--- mistsystems/requests/sites/psks.py
+++ mistsystems/requests/sites/psks.py
@@ -3,32 +3,61 @@ class Psks():
     def __init__(self, session):
         self.session = session
 
-    def create(self, site_id, psk):
+    def create(self, site_id, psk_settings):
+        """
+        Create a new Personal PSK
+        Parameters:
+            site_id: String
+            psk_settings: Dict
+                name: string: a name for the PSK
+                passphrase: string: passphrase of the PSK (8-63 character or 64 in hex)
+                ssid: string: SSID this PSK should be applicable to
+                usage: string: multi (default) / single
+                vlan_id: int: VLAN for this PSK key
+                mac: string: if usage==single, the mac that this PSK ties to, empty if auto-binding
+        """
         uri = "/api/v1/sites/%s/psks" % site_id
-        resp = self.session.mist_post(uri, body=psk)
+        resp = self.session.mist_post(uri, body=psk_settings)
         return resp
 
-    def get(self, site_id, psk_id="", name="", ssid="", page=1, limit=100):
+    def get(self, site_id, name=None, ssid=None, page=1, limit=100):
+        """
+        Get list of Personal PSKs
+        Parameters:
+            site_id: String
+            name: String (optional)
+            ssid: String (optional)
+            page: Int
+            limit: Int
+        """
         uri = "/api/v1/sites/%s/psks" % site_id
         query = {}
-        if psk_id != "":
-            uri += "/%s" % psk_id
-        if name != "":
+        if name:
             query["name"] = name
-        if ssid != "":
+        if ssid:
             query["ssid"] = ssid
         resp = self.session.mist_get(uri, query=query, page=page, limit=limit)
         return resp
 
-    def delete(self, site_id, psk_id="", name="", ssid=""):
-        uri = "/api/v1/sites/%s/psks" % site_id
-        if psk_id != "":
-            uri += "/%s" % psk_id
-        if site_id != "" and ssid != "":
-            uri += "?name=%s&ssid=%s" % (name, ssid)
-        elif name != "":
-            uri += "?name=%s" % name
-        elif ssid != "":
-            uri += "?ssid=%s" % ssid
+    def get_details(self, site_id, psk_id):
+        """
+        Get one Personal PSK
+        Parameters:
+            site_id: String
+            psk_id: String
+        """
+        uri = "/api/v1/sites/{0}/psks/{1}".format(site_id, psk_id)
+        resp = self.session.mist_get(uri)
+        return resp
+
+
+    def delete(self, site_id, psk_id):
+        """
+        Delete one Personal PSK
+        Parameters:
+            site_id: String
+            psk_id: String
+        """
+        uri = "/api/v1/sites/{0}/psks/{1}".format(site_id, psk_id)
         resp = self.session.mist_delete(uri)
         return resp
