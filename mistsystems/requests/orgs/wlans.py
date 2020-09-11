e53e16e5bca5ef2b416abf89768591e6de1063a7
Thomas Munzer <thomas.munzer@gmail.com>
2020-09-11 19:38:39 +0200

    add requests

--- mistsystems/requests/orgs/wlans.py
+++ mistsystems/requests/orgs/wlans.py
@@ -4,16 +4,72 @@ class Wlans():
         self.session = session
 
     def get(self, org_id, page=1, limit=100):
+        """
+        Get list of Wlans
+        Parameters:
+            org_id: String
+            page: Int
+            limit: Int
+        """
         uri = "/api/v1/orgs/%s/wlans" % org_id
         resp = self.session.mist_get(uri, page=page, limit=limit)
         return resp
 
+    def get_derived(self, org_id, resolve, page=1, limit=100):
+        """
+        Get list of Wlans derived
+        Parameters:
+            org_id: String
+            resolve: Boolean (whether to resolve SITE_VARS, default is false)
+            page: Int
+            limit: Int
+        """
+        uri = "/api/v1/orgs/{0}/wlans/derived".format(org_id)
+        query = {"resolve": resolve} if resolve else {}
+        resp = self.session.mist_get(uri, query, page=page, limit=limit)
+        return resp
+
+    def get_by_id(self, org_id, wlan_id):
+        """
+        Get one Wlans details
+        Parameters:
+            org_id: String
+            wlan_id: String
+        """
+        uri = "/api/v1/orgs/{0}/wlans/{1}".format(org_id, wlan_id)
+        resp = self.session.mist_get(uri)
+        return resp
+
     def create(self, org_id, wlan_settings):
+        """
+        Create a Wlan
+        Parameters:
+            org_id: String
+            wlan_settings: Dict (see API documentation)
+        """
         uri = "/api/v1/orgs/%s/wlans" % org_id
         resp = self.session.mist_post(uri, body=wlan_settings)
         return resp
 
+    def update(self, org_id, wlan_id, wlan_settings):
+        """
+        Update a Wlan
+        Parameters:
+            org_id: String
+            wlan_id: String
+            wlan_settings: Dict (see API documentation)
+        """
+        uri = "/api/v1/orgs/{0}/wlans/{1}".format(org_id, wlan_id)
+        resp = self.session.mist_put(uri, body=wlan_settings)
+        return resp
+
     def delete(self, org_id, wlan_id):
+        """
+        Delete a Wlan
+        Parameters:
+            org_id: String
+            wlan_id: String
+        """
         uri = "/api/v1/orgs/%s/wlans/%s" % (org_id, wlan_id)
         resp = self.session.mist_delete(uri)
         return resp
@@ -29,8 +85,9 @@ class Wlans():
         resp = self.session.mist_delete(uri)
         return resp
 
-    def set_portal_template(self, org_id, wlan_id, portal_template_body):
+    def update_portal_template(self, org_id, wlan_id, portal_template_body):
         uri = "/api/v1/orgs/%s/wlans/%s/portal_template" % (org_id, wlan_id)
         body = portal_template_body
         resp = self.session.mist_put(uri, body=body)
         return resp
+
